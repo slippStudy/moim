ba1896eb962c8740928e8005160aa747354abb43
yangwansu <yangwansu@coupang.com>
2020-01-14 23:32:50 +0900

    apply spring boot web

---
+++ src/main/java/net/slipp/franchise/presantations/HelloController.java
@@ -0,0 +1,13 @@
+package net.slipp.franchise.presantations;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world!";
+    }
+}
