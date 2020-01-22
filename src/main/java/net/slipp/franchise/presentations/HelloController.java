648dd28a837738b218cff410c182959e2f9e1470
yangwansu <yangwansu@coupang.com>
2020-01-22 17:49:11 +0900

    fixed typo

--- src/main/java/net/slipp/franchise/presentations/HelloController.java
+++ src/main/java/net/slipp/franchise/presentations/HelloController.java
@@ -1,4 +1,4 @@
-package net.slipp.franchise.presantations;
+package net.slipp.franchise.presentations;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
