78eb345969f267b996bc0c54de7eadbe689159ca
양완수 <ywsaau77@gmail.com>
2020-01-22 18:12:25 +0900

    spring boot web 을 적용한 초기 싱글 프로젝트입니다.  (#5)
    * init java application gradle project
    
    * apply spring boot web
    
    * fixed typo

---
+++ src/test/java/net/slipp/franchise/AppTest.java
@@ -0,0 +1,11 @@
+package net.slipp.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test void contextLoads() {
+    }
+}
