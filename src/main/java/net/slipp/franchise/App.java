8187ad34155def6e61d57ee3e7b6ebfd6e2b3409
LenKIM <joenggyu0@gmail.com>
2020-05-22 16:00:19 +0900

    swagger 연동 작업

--- src/main/java/net/slipp/franchise/App.java
+++ src/main/java/net/slipp/franchise/App.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
     public static void main(String[] args) {
-
         SpringApplication.run(App.class, args);
     }
 }
