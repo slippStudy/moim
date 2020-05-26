31b846570678248d4b659f95db3884dc948828fc
LenKIM <joenggyu0@gmail.com>
2020-05-26 10:14:46 +0900

    remove Validation Email address testcode

--- src/test/java/net/slipp/franchise/controller/RecruitControllerTest.java
+++ src/test/java/net/slipp/franchise/controller/RecruitControllerTest.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -169,13 +171,6 @@ class RecruitControllerTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             Title testTitle = new Title("하");
             recruit.setTitle(testTitle);
-        });
-    }
-
-    @Test
-    public void 유효한_이메일_주소를_가진_사용자가_아니라면_에러를_일으킨다() throws Exception {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-
         });
     }
 
