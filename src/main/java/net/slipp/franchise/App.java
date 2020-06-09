526e569c42efe243ddbc524a92b3d4159380d959
kwangpil.hong <kwangpil.hong@yanolja.com>
2020-06-09 20:21:21 +0900

    liquibase init

--- src/main/java/net/slipp/franchise/App.java
+++ src/main/java/net/slipp/franchise/App.java
@@ -3,8 +3,11 @@ package net.slipp.franchise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ LiquibaseProperties.class })
 public class App {
 
     public static void main(String[] args) {
---
+++ src/main/resources/config/liquibase/sql/sample_init.sql
@@ -0,0 +1,4 @@
+create table sample (
+    id int primary key,
+    name varchar(255)
+);
