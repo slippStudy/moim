4c2ceeb1202bc23b55ec7eeef275203e648e80d3
kwangpil.hong <kwangpil.hong@yanolja.com>
2020-07-24 01:29:06 +0900

    docker-compose mysql 설정 변경

--- recruit/src/main/java/net/slipp/moim/support/config/db/MybatisConfig.java
+++ recruit/src/main/java/net/slipp/moim/support/config/db/MybatisConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan(basePackages = {MybatisConfig.BASE_PACKAGE})
 public class MybatisConfig {
-    public static final String BASE_PACKAGE = "net.slipp.moim";
+    public static final String BASE_PACKAGE = "net.slipp.moim.domain";
     public static final String CONFIG_LOCATION_PATH = "classpath:mybatis/mybatis-config.xml";
     public static final String SQL_LOCATION_PATH = "classpath:/mapper/**/*.xml";
 }
--- recruit/src/test/java/net/slipp/moim/domain/model/sample/SampleMapperTest.java
+++ recruit/src/test/java/net/slipp/moim/domain/model/sample/SampleMapperTest.java
@@ -4,6 +4,7 @@ import net.slipp.moim.support.config.db.DatasourceConfig;
 import net.slipp.moim.support.config.db.MybatisConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,7 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @ContextConfiguration(classes = {MybatisConfig.class, DatasourceConfig.class})
-@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(classes = SampleMapper.class)
 class SampleMapperTest {
 
     @Autowired
@@ -24,6 +26,9 @@ class SampleMapperTest {
     void name() {
         List<Sample> result = sampleMapper.findAll();
 
-        assertThat(result).isNotEmpty();
+        assertThat(result.get(0).getName()).isEqualTo("sample1");
+        assertThat(result.get(0).getAddress()).isEqualTo("테스트 지역");
+        assertThat(result.get(1).getName()).isEqualTo("sample2");
+        assertThat(result.get(1).getAddress()).isEqualTo("테스트 지역");
     }
 }
